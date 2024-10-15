c74a295bf1750c1d059bc7adb008e86b3024fc53
CodifyArhab <fdzakyarhab@gmail.com>
2024-10-15 13:07:18 +0700

    update

---
+++ UTSMobile/app/src/main/java/com/fadhiil2010/uts_mobile/Model/ItemModel.kt
@@ -0,0 +1,9 @@
+package com.fadhiil2010.uts_mobile.Model
+
+data class ItemModel(
+    var namaMenu : String,
+    var imageMenu : Int,
+    var Tanggal :String,
+    var Harga : String,
+    var totalItem : String
+)
--- UTSMobile/app/src/main/java/com/fadhiil2010/uts_mobile/Order.kt
+++ UTSMobile/app/src/main/java/com/fadhiil2010/uts_mobile/Order.kt
@@ -5,12 +5,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.fadhiil2010.uts_mobile.Adapter.ItemAdapter
+import com.fadhiil2010.uts_mobile.Model.ItemModel
 
 class Order : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var iconAdapter: ItemAdapter
+    private var itemList = mutableListOf<ItemModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_order)
+
+        recyclerView = findViewById(R.id.rvOrder)
+        //ini kita bikin array data
+        val listBuku = listOf(
+            ItemModel(namaMenu = "Chicken Curry", R.drawable.curry, "29 Nov, 01:20 PM", "$50.50", "2 items"),
+            ItemModel(namaMenu = "Burger", R.drawable.burger, "29 Nov, 01:20 PM", "$50.50", "2 items"),
+            ItemModel(namaMenu = "Coffe Latte", R.drawable.coffe, "29 Nov, 01:20 PM", "$50.50", "2 items"),
+
+        )
+        val nBukuAdapter = ItemAdapter(listBuku)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@Order)
+            adapter = nBukuAdapter
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
