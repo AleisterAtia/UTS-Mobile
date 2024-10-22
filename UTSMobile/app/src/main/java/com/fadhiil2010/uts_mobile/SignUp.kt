54f2b673ebdaf25f732b64f60d61fae7850748bd
CodifyArhab <fdzakyarhab@gmail.com>
2024-10-22 21:24:59 +0700

    update

--- UTSMobile/app/src/main/java/com/fadhiil2010/uts_mobile/SignUp.kt
+++ UTSMobile/app/src/main/java/com/fadhiil2010/uts_mobile/SignUp.kt
@@ -1,6 +1,8 @@
 package com.fadhiil2010.uts_mobile
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,13 @@ class SignUp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up)
+
+        val ActButton1 = findViewById<TextView>(R.id.tvLogIn)
+        ActButton1.setOnClickListener {
+            val Intent = Intent(this, LogIn::class.java)
+            startActivity(Intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
